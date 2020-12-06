b1fb43bcc194d331b6dae071e898cb052bd67a52
Mariamgad <47721226+Mariamgad@users.noreply.github.com>
2020-12-06 20:13:16 +0200

    Create decimalToBase.cpp

---
+++ Practice/decimalToBase.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define input ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define ii pair<int,int>
+#define pll pair<ll,ll>
+#define dl endl
+#define ft first
+#define sc second
+using namespace std;
+stack<char>st;
+string s;
+string decimalToBase(int num, int base)
+{
+    int rem;
+    while (num > 0)
+    {
+        rem = num % base;
+        num /= base;
+        st.push(rem + '0');
+    }
+    while (!st.empty())
+    {
+        char x = st.top(); st.pop();
+        s += x;
+    }
+    return s;
+}
+int main()
+{
+    input;
+    int n,base;
+}
